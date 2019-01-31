189c6b4c5ddb1a2ea2a4b1d367e3ece9ceaffdfa
Adeyinka <adeyinkaadejumobi.com>
2019-01-31 13:30:11 +0100

    changed file

--- Promises/promises1.js
+++ Promises/promises1.js
@@ -4,4 +4,22 @@
 // is in your array. It should return a Promise that will be 
 // accepted or rejected if the value is inside the array or not.
 
-let favFruit = [Banana, ]
+const myFavFruit = (fruit)=>{
+    // List of fruits
+    let favFruit = ["banana", "orange", "water Lemon", "apple", "pawpaw"];
+    let fruitIndex = favFruit.indexOf(fruit);
+    // checking fruit in the list
+    if (fruitIndex !== -1){
+      return Promise.resolve(fruit + " is one of my favourite fruits");
+    }
+    return Promise.reject(fruit + " is not my favourite fruit");
+  }
+  async function favourite(){
+    try{
+      let rightFruit = await myFavFruit("apple");
+      console.log(rightFruit);
+    }catch(error){
+      console.log(error);
+    }
+  }
+  favourite();
